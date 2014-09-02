a6709382e7b94a0983f746aa47ced512bc78a97e
shah <shahroozsanei@gmail.com>
2014-09-02 16:46:29 +0430

    add setting field and default value

--- acf-typography-v5.php
+++ acf-typography-v5.php
@@ -14,6 +14,11 @@ class acf_field_typography extends acf_field {
 	*  @return	n/a
 	*/
 	
+	//=========================================================================================
+	//=================== This function will setup the field type data ========================
+	//=========================================================================================
+
+
 	function __construct() {
 		
 		/*
@@ -34,16 +39,68 @@ class acf_field_typography extends acf_field {
 		/*
 		*  defaults (array) Array of default settings which are merged into the field object. These are used later in settings
 		*/
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//=========================== get json for seting ===========================
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+		$json = file_get_contents('http://localhost/wordpress/wp-content/plugins/acf-typography/gf.json');
+		$fontArray = json_decode( $json);
 		
+		$font_familys = array();
+		foreach ( $fontArray as $k => $v ) {
+			if (is_array($v))
+			{
+			    foreach ($v as $value)
+			    {
+			       foreach ($value as $key1 => $value1) {
+			       	if($key1== 'family')
+			        	array_push($font_familys, $value1); 
+			       }
+			    }
+			}
+		}
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//======     defaults (array) Array of default settings which are       =====
+	//====== merged into the field object. These are used later in settings =====
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 		$this->defaults = array(
-			'font_size'	=> 14,
+			'font_size'		=> 20,
+			'line_height'	=> 25,
 			'default_value'	=> '',
 			'new_lines'		=> '',
 			'maxlength'		=> '',
 			'placeholder'	=> '',
 			'readonly'		=> 0,
 			'disabled'		=> 0,
-			'rows'			=> ''
+			'rows'			=> '',
+			'line_height'	=> 25,
+			'font_familys'	=> $font_familys,
+			'stylefont'	 	=> array( '300','400', '600','700','800'),
+			'backupfont'	=> array(
+            	"Arial, Helvetica, sans-serif"                          => "Arial, Helvetica, sans-serif",
+            	"'Arial Black', Gadget, sans-serif"                     => "'Arial Black', Gadget, sans-serif",
+            	"'Bookman Old Style', serif"                            => "'Bookman Old Style', serif",
+            	"'Comic Sans MS', cursive"                              => "'Comic Sans MS', cursive",
+            	"Courier, monospace"                                    => "Courier, monospace",
+            	"Garamond, serif"                                       => "Garamond, serif",
+            	"Georgia, serif"                                        => "Georgia, serif",
+            	"Impact, Charcoal, sans-serif"                          => "Impact, Charcoal, sans-serif",
+            	"'Lucida Console', Monaco, monospace"                   => "'Lucida Console', Monaco, monospace",
+            	"'Lucida Sans Unicode', 'Lucida Grande', sans-serif"    => "'Lucida Sans Unicode', 'Lucida Grande', sans-serif",
+            	"'MS Sans Serif', Geneva, sans-serif"                   => "'MS Sans Serif', Geneva, sans-serif",
+            	"'MS Serif', 'New York', sans-serif"                    => "'MS Serif', 'New York', sans-serif",
+            	"'Palatino Linotype', 'Book Antiqua', Palatino, serif"  => "'Palatino Linotype', 'Book Antiqua', Palatino, serif",
+            	"Tahoma,Geneva, sans-serif"                             => "Tahoma, Geneva, sans-serif",
+            	"'Times New Roman', Times,serif"                        => "'Times New Roman', Times, serif",
+            	"'Trebuchet MS', Helvetica, sans-serif"                 => "'Trebuchet MS', Helvetica, sans-serif",
+            	"Verdana, Geneva, sans-serif"                           => "Verdana, Geneva, sans-serif",
+        	)
 		);
 
 
@@ -64,32 +121,166 @@ class acf_field_typography extends acf_field {
     	
 	}
 	
-	
-	/*
-	*  render_field_settings()
-	*
-	*  Create extra settings for your field. These are visible when editing a field
-	*
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$field (array) the $field being edited
-	*  @return	n/a
-	*/
-	
+
+
+
+	//=========================================================================================
+	//============================ extra settings for field ===================================
+	//=========================================================================================
+
 	function render_field_settings( $field ) {
+
+
+
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Font Family ?','acf-typography'),
+			'type'			=> 'radio',
+			'layout'		=> 'horizontal',
+			'name'			=> 'font_familys_?',
+			'choices'		=>	array(
+								1	=>	__('Yes','acf-font-awesome'),
+								0	=>	__('No','acf-font-awesome')
+							)
+		));
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Font Family','acf-typography'),
+			'type'			=> 'select',
+			'name'			=> 'default_font_familys',
+			'choices'		=>	$field['font_familys']
+		));
+
+
+
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Style Font ?','acf-typography'),
+			'type'			=> 'radio',
+			'layout'		=>  'horizontal',
+			'name'			=> 'stylefont_?',
+			'choices'		=>	array(
+								1	=>	__('Yes','acf-font-awesome'),
+								0	=>	__('No','acf-font-awesome')
+							)
+		));
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Style Font','acf-typography'),
+			'type'			=> 'select',
+			'name'			=> 'default_stylefont',
+			'choices'		=>	$field['stylefont']
+		));
+
+
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Backup Font ?','acf-typography'),
+			'type'			=> 'radio',
+			'layout'		=>  'horizontal',
+			'name'			=> 'backupfont_?',
+			'choices'		=>	array(
+								1	=>	__('Yes','acf-font-awesome'),
+								0	=>	__('No','acf-font-awesome')
+							)
+		));
+
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Backup Font','acf-typography'),
+			'type'			=> 'select',
+			'name'			=> 'default_backupfont',
+			'choices'		=>	$field['backupfont']
+		));
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Text Align ?','acf-typography'),
+			'type'			=> 'radio',
+			'layout'  		=>  'horizontal',
+			'name'			=> 'text_align_?',
+			'choices'		=>	array(
+									1	=>	__('Yes','acf-font-awesome'),
+									0	=>	__('No','acf-font-awesome')
+								)
+		));
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Text Align','acf-typography'),
+			'type'			=> 'select',
+			'name'			=> 'default_text_align',
+			'choices'		=>	array('inherit', 'left', 'right', 'center', 'justify', 'inital')
+		));
+
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Text direction ?','acf-typography'),
+			'type'			=> 'radio',
+			'layout'  		=>  'horizontal',
+			'name'			=> 'text_direction_?',
+			'choices'		=>	array(
+								1	=>	__('Yes','acf-font-awesome'),
+								0	=>	__('No','acf-font-awesome')
+							)
+		));
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Text direction','acf-typography'),
+			'type'			=> 'select',
+			'name'			=> 'default_text_align',
+			'choices'		=>	array( 'rtl' => 'right to left',
+								   'ltr' => 'left to right',)
+		));
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Font Size ?','acf-typography'),
+			'type'			=> 'radio',
+			'layout'  		=>  'horizontal',
+			'name'			=> 'font_size_?',
+			'choices'		=>	array(
+								1	=>	__('Yes','acf-font-awesome'),
+								0	=>	__('No','acf-font-awesome')
+							)
+		));
+
 
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Font Size','acf-typography'),
-			'instructions'	=> __('Customise the input font size','acf-typography'),
 			'type'			=> 'number',
 			'name'			=> 'font_size',
 			'prepend'		=> 'px',
 		));
 
+
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Line Height ?','acf-typography'),
+			'type'			=> 'radio',
+			'layout'  		=>  'horizontal',
+			'name'			=> 'line_height_?',
+			'choices'		=>	array(
+									1	=>	__('Yes','acf-font-awesome'),
+									0	=>	__('No','acf-font-awesome')
+								)
+		));
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Line Height','acf-typography'),
+			'type'			=> 'number',
+			'name'			=> 'line_height',
+			'prepend'		=> 'px',
+		));
+
+
+
+
 	}
 
+
+
+
+	//=========================================================================================
+	//============================== show seting in field =====================================
+	//=========================================================================================
 
 	function render_field( $field ) {
 		// convert value to array
@@ -102,7 +293,7 @@ class acf_field_typography extends acf_field {
             $field['value'][''] = '';
             $field['value']['font-family']	 = 	'';
             $field['value']['font-weight']	 = 	'';
-            $field['value']['backup_font']	 = 	'Tahoma,Geneva, sans-serif';
+            $field['value']['backupfont']	 = 	'Tahoma,Geneva, sans-serif';
             $field['value']['text_align']	 =	'left';
             $field['value']['font_size']	 =	'20';
             $field['value']['line_height']	 =	'25';
@@ -110,13 +301,6 @@ class acf_field_typography extends acf_field {
         }
 
         $field_value = $field['value'];
-
-		
-		/*
-		*  Create a simple text input using the 'font_size' setting.
-		*/
-		$json = file_get_contents('http://localhost/wordpress/wp-content/plugins/acf-typography/gf.json');
-		$fontArray = json_decode( $json);
 
 		echo "<style id='preview_style' ></style>";
 
@@ -127,25 +311,15 @@ class acf_field_typography extends acf_field {
 		$s = 0;
 		$e = '';
 
-		$defaults_fonts = array(
-            "Arial, Helvetica, sans-serif"                          => "Arial, Helvetica, sans-serif",
-            "'Arial Black', Gadget, sans-serif"                     => "'Arial Black', Gadget, sans-serif",
-            "'Bookman Old Style', serif"                            => "'Bookman Old Style', serif",
-            "'Comic Sans MS', cursive"                              => "'Comic Sans MS', cursive",
-            "Courier, monospace"                                    => "Courier, monospace",
-            "Garamond, serif"                                       => "Garamond, serif",
-            "Georgia, serif"                                        => "Georgia, serif",
-            "Impact, Charcoal, sans-serif"                          => "Impact, Charcoal, sans-serif",
-            "'Lucida Console', Monaco, monospace"                   => "'Lucida Console', Monaco, monospace",
-            "'Lucida Sans Unicode', 'Lucida Grande', sans-serif"    => "'Lucida Sans Unicode', 'Lucida Grande', sans-serif",
-            "'MS Sans Serif', Geneva, sans-serif"                   => "'MS Sans Serif', Geneva, sans-serif",
-            "'MS Serif', 'New York', sans-serif"                    => "'MS Serif', 'New York', sans-serif",
-            "'Palatino Linotype', 'Book Antiqua', Palatino, serif"  => "'Palatino Linotype', 'Book Antiqua', Palatino, serif",
-            "Tahoma,Geneva, sans-serif"                             => "Tahoma, Geneva, sans-serif",
-            "'Times New Roman', Times,serif"                        => "'Times New Roman', Times, serif",
-            "'Trebuchet MS', Helvetica, sans-serif"                 => "'Trebuchet MS', Helvetica, sans-serif",
-            "Verdana, Geneva, sans-serif"                           => "Verdana, Geneva, sans-serif",
-        );
+		$defaults_fonts = $field['backupfont'];
+
+
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//========================== show render field ==============================
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 		echo "<div>";
 
@@ -168,9 +342,9 @@ class acf_field_typography extends acf_field {
 			echo "<div>";
 
 				//Backup Font Family
-				echo '<select name="' . $field['name'] . '[backup_font]" class = "js-select2">';
+				echo '<select name="' . $field['name'] . '[backupfont]" class = "js-select2">';
 					foreach ( $defaults_fonts as $k => $v ) {
-						echo '<option value="' . $k . '"' . selected($field_value['backup_font'], $k, false) . ' >' . $k . '</option>' ;
+						echo '<option value="' . $k . '"' . selected($field_value['backupfont'], $k, false) . ' >' . $k . '</option>' ;
 					}
 				echo '</select>';
 
@@ -217,21 +391,17 @@ class acf_field_typography extends acf_field {
   				</div>';
 	}
 		
-	/*
-	*  input_admin_enqueue_scripts()
-	*
-	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
-	*  Use this action to add CSS + JavaScript to assist your render_field() action.
-	*
-	*  @type	action (admin_enqueue_scripts)
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	n/a
-	*  @return	n/a
-	*/
 
-	
+
+
+	//=========================================================================================
+	//=====         This action is called in the admin_enqueue_scripts action            ======
+	//=====               on the edit screen where your field is created.                ======
+	//===== Use this action to add CSS + JavaScript to assist your render_field() action.======
+	//=========================================================================================
+
+
+
 	function input_admin_enqueue_scripts() {
 		
 		$dir = plugin_dir_url( __FILE__ );
